d7ab3908d8d55ff3d13c7c0bf486f944184afca9
Konstantinos Kyratsous <kyratsous26@gmail.com>
2022-02-02 12:35:09 +0200

    Initial commit

---
+++ src/main/java/com/kyratsous/runnersapp/RunnersAppApplication.java
@@ -0,0 +1,13 @@
+package com.kyratsous.runnersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyratsous/runnersapp/RunnersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyratsous.runnersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
